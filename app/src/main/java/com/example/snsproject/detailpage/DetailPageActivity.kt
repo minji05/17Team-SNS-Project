eab552962f6d7d53b19527aeda06c04c5cfc36bc
LDH <edh7054@gmail.com>
2023-08-14 11:06:34 +0900

    Project Push

---
+++ app/src/main/java/com/example/snsproject/detailpage/DetailPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.snsproject.detailpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.snsproject.R
+
+class DetailPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_page)
+    }
+}
---
+++ app/src/main/java/com/example/snsproject/signinanduppage/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.snsproject.signinanduppage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.snsproject.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
