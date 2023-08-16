c4707025084763cfcc9f487da0d8d947c64cfb25
sooj36 <sooj2023@gmail.com>
2023-08-16 23:37:14 +0900

    [FEAT] : ⚙️ ADD 버튼 클릭하면, MAIN -> DETAIL 로/ main.xml ScrollView + LinearLayout

--- app/src/main/java/com/example/snsproject/MainActivity.kt
+++ app/src/main/java/com/example/snsproject/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.snsproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ScrollView
+import com.example.snsproject.detailpage.DetailPageActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val to_detail_button = findViewById<Button>(R.id.to_detail_button)
+        to_detail_button.setOnClickListener {
+            val intent = Intent(this, DetailPageActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
