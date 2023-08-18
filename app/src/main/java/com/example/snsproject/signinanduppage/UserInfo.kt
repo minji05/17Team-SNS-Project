e37501eed1e1173fbd722b778fb96248a27d1637
LDH <edh7054@gmail.com>
2023-08-19 00:34:34 +0900

    UserInfo @Parcelize commit

--- app/src/main/java/com/example/snsproject/signinanduppage/UserInfo.kt
+++ app/src/main/java/com/example/snsproject/signinanduppage/UserInfo.kt
@@ -1,41 +1,13 @@
 package com.example.snsproject.signinanduppage
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class UserInfo(
     val name: String,
     val id: String,
     val password: String,
     val mbti: String
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: ""
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeString(id)
-        parcel.writeString(password)
-        parcel.writeString(mbti)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<UserInfo> {
-        override fun createFromParcel(parcel: Parcel): UserInfo {
-            return UserInfo(parcel)
-        }
-
-        override fun newArray(size: Int): Array<UserInfo?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
 
